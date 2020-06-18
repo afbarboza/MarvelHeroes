40f12453d2e0789ead70f2f3c7cda1e9db6ee7f6
Alex <alex.barboza@usp.br>
2020-06-17 22:24:22 -0300

    created splash screen horizontal animation

--- app/src/main/java/com/example/marvelapp/MainActivity.java
+++ app/src/main/java/com/example/marvelapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.marvelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -19,6 +21,7 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private TextView txtAppName;
 
     private Call<CharacterDataWrapper> getAllCharacters;
 
@@ -26,8 +29,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initRetrofit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initViews();
+    }
+
+    private void initViews() {
+        this.txtAppName = findViewById(R.id.txtAppName);
+
+
+        float screenWidth = (1.0f * this.getResources().getDisplayMetrics().widthPixels);
+        float textWidth = (float) (1.0f * this.txtAppName.getWidth());
+        float middleCoordinate = (0.5f * (screenWidth - textWidth)) - (0.25f * screenWidth);
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(this.txtAppName, "translationX", middleCoordinate);
+        animator.setDuration(250);
+        animator.start();
     }
 
     private void initRetrofit() {
