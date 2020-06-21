ef73ca1c2527e8aea4eef9a8599457da951da0f0
Alex <alex.barboza@usp.br>
2020-06-21 13:34:22 -0300

    splash screen progress bar animation done

--- app/src/main/java/com/example/marvelapp/MainActivity.java
+++ app/src/main/java/com/example/marvelapp/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.marvelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import Controller.CallbackCharacterDataWrapper;
@@ -15,7 +19,10 @@ import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
     private TextView txtAppName;
+    private ProgressBar loadingProgressBar;
+    private ProgressBarThread progressBarThread;
 
     private Call<CharacterDataWrapper> getAllCharacters;
 
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.txtAppName = findViewById(R.id.txtAppName);
+        this.loadingProgressBar = findViewById(R.id.progressBar);
         initRetrofit();
     }
 
@@ -31,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         animateSplashScreen();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (this.progressBarThread != null) {
+            this.progressBarThread.cancel(true);
+            this.progressBarThread = null;
+            System.gc();
+        }
     }
 
     private void animateSplashScreen() {
@@ -45,12 +63,36 @@ public class MainActivity extends AppCompatActivity {
         Animation animation = new AlphaAnimation(0.0f, 1.0f);
         animation.setDuration(2000);
         this.txtAppName.startAnimation(animation);
+
+        progressBarThread = new ProgressBarThread();
+        progressBarThread.execute();
     }
-
-
 
     private void initRetrofit() {
         this.getAllCharacters = MarvelClient.getMarvelEndpointAPI().getAllCharacters();
         this.getAllCharacters.enqueue(new CallbackCharacterDataWrapper(this));
     }
+
+    private class ProgressBarThread extends AsyncTask<Void, Void, Void> {
+        private static final String TAG = "ProgressBarThread";
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                Log.d(TAG, "doInBackground: " + e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Animation animation = new AlphaAnimation(0.0f, 1.0f);
+            animation.setDuration(500);
+            loadingProgressBar.setAnimation(animation);
+            loadingProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
