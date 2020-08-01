48f0dd447fd01164ba8cf25ca89cbed81192bff7
Alex <alex.barboza@usp.br>
2020-08-01 18:01:18 -0300

    created gridlayout in the gallery

--- app/src/main/java/Controller/BooksGridAdapter.java
+++ app/src/main/java/Controller/BooksGridAdapter.java
@@ -1,4 +1,42 @@
 package Controller;
 
-public class BooksGridAdapter {
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import Model.CharacterCollection;
+
+public class BooksGridAdapter extends BaseAdapter {
+    private final Context mContext;
+
+    public BooksGridAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return CharacterCollection.getCharactersList().size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return CharacterCollection.getCharactersList().get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return CharacterCollection.getCharactersList().get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView txtHeroName = new TextView(mContext);
+        String heroName = CharacterCollection.getCharactersList().get(position).getName();
+        txtHeroName.setText(heroName);
+        txtHeroName.setTextColor(Color.parseColor("#000000"));
+        return txtHeroName;
+    }
 }
--- app/src/main/java/com/example/marvelapp/HeroesGallery.java
+++ app/src/main/java/com/example/marvelapp/HeroesGallery.java
@@ -2,12 +2,25 @@ package com.example.marvelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import Controller.BooksGridAdapter;
 
 public class HeroesGallery extends AppCompatActivity {
+
+    private GridView gridView;
+    private BooksGridAdapter booksGridAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroes_gallery);
+
+        gridView = findViewById(R.id.gridView);
+        booksGridAdapter = new BooksGridAdapter(this);
+        gridView.setAdapter(booksGridAdapter);
     }
+
+    @Override
+    public void onBackPressed() {}
 }
